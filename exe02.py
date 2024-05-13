534aae227052c1c9d1e4ef1aacfef46070865c1a
Moshe Winberg <WIMOSHE3@365.openu.ac.il>
2024-05-13 14:03:08 +0300

    fixing naming of pics

--- exe02.py
+++ exe02.py
@@ -2,24 +2,77 @@ import requests
 from bs4 import BeautifulSoup
 import bs4
 
+
+def strip_slash(txt):
+    if txt.rfind('/') != -1:
+        return txt[txt.rfind('/'):]
+    else:
+        return txt
+            
+
+
+def pic_name(tale): 
+     img_name = tale.partition('px-')
+     
+     if img_name.count('px-') != 0:
+     #   return img_name[img_name.index('px-')+1]
+        return strip_slash(img_name[img_name.index('px-')+1])
+     else:
+         return strip_slash(img_name[0])
+
+"""
+    there is three data-sets
+    1. imgTable: contains all implementations of images 
+    2. txt: contains the splited txt of the cueernt implementation
+    3. tablet: contains all the images of the current type
+"""
 def pics_scraper(web):                                  #pics scraper
     
     soup = BeautifulSoup(web.content)
 
-    table = soup.find_all('img')
+    imgTable = soup.find_all('img')
 
-    for i in range(len(table)):
+    for i in range(len(imgTable)):                      # loop over all pics links
     
-        txt = str(table[i])
+        txt = str(imgTable[i])
         txt = txt.split()
 
-        tablet = list()                                 #creat a list
+        tablet = list()                                 # creat a list
 
     
-        for x in range(len(txt)):                       #Puts the drity pics links in the temp tablet list
+        for x in range(len(txt)):                       # Puts the drity pics links in the temp tablet list
             if txt[x].count('//upload') != 0:
                 tablet.append(txt[x])       
-    
+            
+        print(tablet)
+            
+        for x in range(len(tablet)):
+            tale = tablet[x].partition('//')
+            tale = tale[tale.index('//')+1]
+
+            tablet[x] = head + tale                    #
+            tablet[x] = tablet[x].rstrip('"')
+            
+            if x == len(tablet)-1:
+                
+               #img_name = str(i) + '_' + str(x) + '.jpg'  
+               # img_name = tale.partition('px-')
+                #img_name = img_name[img_name.index('px-')+1]
+                img_name = pic_name(tale)            
+                img_data = requests.get(tablet[x]).content
+                with open(pic_name(tale), 'wb') as handler:
+                    handler.write(img_data)
+      
+            
+        """"
+            img_name = str(i) + '_' + str(x) + '.jpg'  
+            img_data = requests.get(tablet[x]).content
+            with open(img_name, 'wb') as handler:
+                handler.write(img_data)
+        """
+         
+            
+        """
         for x in range(len(tablet)):
             tale = tablet[x].partition('//')
             tale = tale[tale.index('//')+1]
@@ -32,7 +85,7 @@ def pics_scraper(web):                                  #pics scraper
             img_data = requests.get(tablet[x]).content
             with open(img_name, 'wb') as handler:
                 handler.write(img_data)
-
+        """
 
 #################
 def links_scraper(web):                                 #links scraper
